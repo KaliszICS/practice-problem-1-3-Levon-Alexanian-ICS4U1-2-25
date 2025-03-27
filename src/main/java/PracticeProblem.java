3e955b4339f727d5caa3c4ab217a47eb7eed8478
Levon-Alexanian-ICS4U1-2-25 <levon.alexanian@student.tdsb.on.ca>
2025-03-27 05:25:10 +0000

    Update PracticeProblem.java

--- src/main/java/PracticeProblem.java
+++ src/main/java/PracticeProblem.java
@@ -1,9 +1,27 @@
+import java.io.*; 
+
 public class PracticeProblem {
 
-	public static void main(String args[]) {
+	public static String readFile(String filename) throws IOException {
+        StringBuilder fileContent = new StringBuilder();
+        FileReader fr = new FileReader(filename);
+        int character;
+        while ((character = fr.read()) != -1) {
+            fileContent.append((char) character);
+        }
+        fr.close();
+        return fileContent.toString();
+    }
 
-	}
-
-	
+    public static String backwardsReadFile(String filename) throws IOException {
+        StringBuilder fileContent = new StringBuilder();
+        FileReader fr = new FileReader(filename);
+        int character;
+        while ((character = fr.read()) != -1) {
+            fileContent.insert(0, (char) character);  
+        }
+        fr.close();
+        return fileContent.toString();
+    }
 
 }
